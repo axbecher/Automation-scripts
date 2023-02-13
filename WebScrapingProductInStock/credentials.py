2847d07990844d063b87d245bdc439bbc4209328
IceFlamesStudio <ice-flamesstudio@outlook.com>
2023-02-13 14:29:34 +0200

    update

--- WebScrapingProductInStock/credentials.py
+++
@@ -1,8 +0,0 @@
-# User email:
-email_user = 'outlookEmail'
-# User password:
-email_password = 'passwordEmail'
-# Server for Office, details -> https://domar.com/pages/smtp_pop3_server
-server = 'smtp-mail.outlook.com'
-# Who will receive this email / output / html / table ?
-to = 'whoWillRecevie'
